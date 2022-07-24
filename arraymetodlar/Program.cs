250cc24b1c6c07ebc2d80e1b19e880ba59c4685c
seydadilandursun <108757360+seydadilandursun@users.noreply.github.com>
2022-07-24 11:30:53 +0300

    arraymetodları

---
+++ arraymetodlar/Program.cs
@@ -0,0 +1,39 @@
+﻿//Sort
+int[] sayiDizisi={23,12,4,86,72,3,11,17};
+
+Console.WriteLine("Sirasiz Dizi");
+foreach (var sayi in sayiDizisi)
+Console.WriteLine(sayi);
+
+Console.WriteLine("Sirali Dizi");
+Array.Sort(sayiDizisi);
+
+foreach (var sayi in sayiDizisi)
+Console.WriteLine(sayi);
+
+//Clear
+Console.WriteLine("Array Clear");
+//sayi dizisi elemanlarini kulllanrak 2.indexten itibaren 2 tane elemani 0lar
+Array.Sort(sayiDizisi,2,2);
+foreach (var sayi in sayiDizisi)
+Console.WriteLine(sayi);
+
+//Reverse
+Console.WriteLine("Array Reverse");
+//Listedeki siralama tersine cevrilir
+Array.Reverse(sayiDizisi);
+foreach (var sayi in sayiDizisi)
+Console.WriteLine(sayi);
+
+
+//Inexof
+Console.WriteLine("Index Of");
+Console.WriteLine(Array.IndexOf(sayiDizisi,23));
+
+//Resize 
+Console.WriteLine("Array Resize");
+Array.Resize<int>(ref sayiDizisi,9);
+sayiDizisi[8]=99;
+foreach (var sayi in sayiDizisi)
+Console.WriteLine(sayi);
+
